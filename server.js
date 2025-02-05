4aeba641c4271eac5081f9641205053eda73c5db
Ojas Joshi <joshiojas185@gmail.com>
2025-02-05 14:40:17 +0530

    Commit Rollback

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ wss.on("connection", (ws) => {
     for (let teacher in activeTeachers) {
       if (activeTeachers[teacher] === ws) {
         // Set last seen time to current timestamp when teacher disconnects
-        const lastSeenTime = new Date().toLocaleTimeString;
+        const lastSeenTime = new Date();
 
         db.query("UPDATE teachers SET status='inactive', last_seen=? WHERE name=?", [lastSeenTime, teacher], (err) => {
           if (err) {
